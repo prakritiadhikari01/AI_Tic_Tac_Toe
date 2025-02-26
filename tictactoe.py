331e8b48bc45077a1fc50c8e5cfcf75703e3e3a0
prakritiadhikari01 <prakriti.adhikari.456@gmail.com>
2025-02-26 13:01:30 +0545

    Board Functions Done

--- tictactoe.py
+++ tictactoe.py
@@ -11,14 +11,61 @@ pygame.display.set_caption('Tic Tac Toe AI')
 screen.fill(BG_COLOR)
 
 class Board():
+
     def __init__(self):
+
         self.squares=np.zeros((ROWS,COLS))
+        self.empty_sqrs=self.squares
+        self.marked_sqrs=0
+
+    def final_state(self):
+        '''
+        @return 0 if no win
+        @return 1 if 1 win
+        @return 2 if 2 win
+        '''
+
+        #vertical wins
+        for col in range(COLS):
+            if self.squares[0][col]==self.squares[1][col]==self.squares[2][col] !=0:
+                return self.squares[0][col]
+
+        #horizontal wins
+        for row in range(ROWS):
+            if self.squares[row][0]==self.squares[row][1]==self.squares[row][2] !=0:
+                return self.squares[row][0]
+
+        #desc_diagonal wins
+        if self.squares[0][0]==self.squares[1][1]==self.squares[2][2]!=0:
+            return self.squares[1][1]
+
+        #asc_diagonal wins
+        if self.squares[0][2]==self.squares[1][1]==self.squares[2][0]!=0:
+            return self.squares[1][1]
+
+        #no win yet
+        return 0
 
     def mark_sqr(self,row,col,player):
         self.squares[row][col]=player
+        self.marked_sqrs+=1
 
     def empty_sqr(self,row,col):
         return self.squares[row][col]==0
+
+    def get_empty_sqrs(self):
+        empty_sqrs=[]
+        for row in range(ROWS):
+            for col in range(COLS):
+                if self.empty_sqr(row,col):
+                    empty_sqrs.append((row,col))
+        return empty_sqrs
+
+    def isfull(self):
+        return self.marked_sqrs==9
+
+    def isempty(self):
+        return self.marked_sqrs==0
 class Game:
 
     def __init__(self):
